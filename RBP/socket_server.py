4ab6897829d60f35b2432e7a6cfb55ecc95ebdcb
Will Woodacre <wjw15@ic.ac.uk>
2017-02-04 20:20:49 +0000

    Finished basic socket implementation

--- RBP/socket_server.py
+++ RBP/socket_server.py
@@ -8,3 +8,14 @@ size = 1024
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.bind((macAddr, port))
 s.listen(backlog)
+try:
+	client, address = s.accept()
+	while 1:
+		data = client.recv(size)
+		if data:
+			print(data)
+			client.send(data)
+except:
+	print("Closing socket")
+	client.close()
+	s.close()
