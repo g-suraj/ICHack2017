07d8c0d2c78d20020c559b37c1685664210943a9
Will Woodacre <wjw15@ic.ac.uk>
2017-02-04 22:49:30 +0000

    Socket now tries to send a json entry of all sensors over bluetooth

--- RBP/socket_server.py
+++ RBP/socket_server.py
@@ -1,6 +1,14 @@
 import socket
+import RPi.GPIO as GPIO
+import time
 
-macAddr = 'b8:27:eb:5b:11:0c'
+chan_list = [5,6,13,19,26,16,20,21]
+pin_map = {5 : 'D2', 6 : 'U1', 13 : 'U2', 19 : 'U3', 26 : 'D4',16 : 'D3', 20 : 'D1', 21 : 'U4'}
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(chan_list, GPIO.IN)
+
+
+macAddr = 'b8:27:eb:f1:bb:a6'
 port = 3
 backlog = 1
 size = 1024
@@ -11,10 +19,17 @@ s.listen(backlog)
 try:
 	client, address = s.accept()
 	while 1:
-		data = client.recv(size)
-		if data:
-			print(data)
-			client.send(data)
+		data = "{"
+		first = true
+		for pin in chan_list:
+			if not first:
+				data += ","
+	        	data += "\"" + pin_map[pin] + "\" : "		
+			data += GPIO.input(pin)
+			first = false
+		data += "}"
+		client.send(data)
+		time.wait(1)
 except:
 	print("Closing socket")
 	client.close()
