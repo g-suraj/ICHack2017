b898216787455e300feec210718ac51a3181fcd5
sg6215 <sg6215@ic.ac.uk>
2017-02-04 17:46:42 +0000

    Added link between login screen and bluetooth connection page.

---
+++ IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/LoginScreen.java
@@ -0,0 +1,22 @@
+package ioichack2017.github.httpscontactgsuraj.iop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+
+    /** Called when the user clicks the Sign In button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent myIntent = new Intent(LoginScreen.this, MainActivity.class);
+        LoginScreen.this.startActivity(myIntent);
+    }
+}
--- IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/MainActivity.java
+++ IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/MainActivity.java
@@ -1,17 +1,12 @@
 package ioichack2017.github.httpscontactgsuraj.iop;
 
-import android.graphics.Color;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //TextView t = (TextView)findViewById(R.id.welcome);
-        //t.setTextColor(Color.WHITE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
