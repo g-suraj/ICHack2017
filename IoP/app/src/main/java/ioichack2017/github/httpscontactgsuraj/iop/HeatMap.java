efb7736ce31ea904899b83f2b1e7d19a2acb284b
sg6215 <sg6215@ic.ac.uk>
2017-02-05 11:23:09 +0000

    Partial heat map and markup update.

---
+++ IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/HeatMap.java
@@ -0,0 +1,13 @@
+package ioichack2017.github.httpscontactgsuraj.iop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeatMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heat_map);
+    }
+}
