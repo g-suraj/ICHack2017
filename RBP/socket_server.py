3d3e1a68488136aadcb07db256d190fea1ec600d
Will Woodacre <wjw15@ic.ac.uk>
2017-02-05 02:59:33 +0000

    RBP server now correctly sends data to app through bluetooth

--- RBP/socket_server.py
+++ RBP/socket_server.py
@@ -7,6 +7,7 @@ pin_map = {5 : 'D2', 6 : 'U1', 13 : 'U2', 19 : 'U3', 26 : 'D4',16 : 'D3', 20 : '
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(chan_list, GPIO.IN)
 
+chan_list = [5,6,13,19,26,16,20,21]
 
 macAddr = 'b8:27:eb:f1:bb:a6'
 port = 3
@@ -19,17 +20,20 @@ s.listen(backlog)
 try:
 	client, address = s.accept()
 	while 1:
-		data = "{"
-		first = true
-		for pin in chan_list:
-			if not first:
-				data += ","
-	        	data += "\"" + pin_map[pin] + "\" : "		
-			data += GPIO.input(pin)
-			first = false
-		data += "}"
-		client.send(data)
-		time.wait(1)
+		recieved = client.recv(size)
+		if recieved:
+			print(recieved)
+			data = "{"
+			first = True
+			for i in chan_list:
+				if not first:
+					data += ","
+	        		data += "\"" + `pin_map[i]` + "\" : " 
+				data += `GPIO.input(i)`
+				first = False
+			data += "}"
+			print(data)
+			client.sendall(data)
 except:
 	print("Closing socket")
 	client.close()
