3be93b94aa473dc4f16206ee1ed62f133feb8dc3
sg6215 <sg6215@ic.ac.uk>
2017-02-05 08:58:45 +0000

    Real time pillow

--- IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/WatchActivity.java
+++ IoP/app/src/main/java/ioichack2017/github/httpscontactgsuraj/iop/WatchActivity.java
@@ -1,6 +1,7 @@
 package ioichack2017.github.httpscontactgsuraj.iop;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,11 +11,95 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class WatchActivity extends AppCompatActivity {
-    private Timer timer;
+    //private Timer timer;
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Insert custom code here
+
+            try {
+                PillowSocket ps = PillowSocket.getInstance();
+                PillowState pstate = ps.getPillowState();
+                boolean arr[] = pstate.getState();
+
+                if (arr[0]) {
+                    TextView t = (TextView) findViewById(R.id.u1);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.u1);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[1]) {
+                    TextView t = (TextView) findViewById(R.id.u2);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.u2);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[2]) {
+                    TextView t = (TextView) findViewById(R.id.u3);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.u3);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[3]) {
+                    TextView t = (TextView) findViewById(R.id.u4);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.u4);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[4]) {
+                    TextView t = (TextView) findViewById(R.id.d1);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.d1);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[5]) {
+                    TextView t = (TextView) findViewById(R.id.d2);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.d2);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[6]) {
+                    TextView t = (TextView) findViewById(R.id.d3);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.d3);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+                if (arr[7]) {
+                    TextView t = (TextView) findViewById(R.id.d4);
+                    t.setBackgroundColor(Color.RED);
+                } else {
+                    TextView t = (TextView) findViewById(R.id.d4);
+                    t.setBackgroundColor(Color.WHITE);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Repeat every 2 seconds
+            handler.postDelayed(runnable, 100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        timer = new Timer();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch);
     }
@@ -24,91 +109,11 @@ public class WatchActivity extends AppCompatActivity {
         super.onResume();
 
         // This timer task will be executed every 1 sec.
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
+        // Create the Handler
 
+        // Define the code block to be executed
 
-                try {
-                    PillowState pstate = ps.getPillowState();
-                    boolean arr[] = pstate.getState();
-
-                    if (arr[0]) {
-                        TextView t = (TextView)findViewById(R.id.u1);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.u1);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[1]) {
-                        TextView t = (TextView)findViewById(R.id.u2);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.u2);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[2]) {
-                        TextView t = (TextView)findViewById(R.id.u3);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.u3);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[3]) {
-                        TextView t = (TextView)findViewById(R.id.u4);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.u4);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[4]) {
-                        TextView t = (TextView)findViewById(R.id.d1);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.d1);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[5]) {
-                        TextView t = (TextView)findViewById(R.id.d2);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.d2);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[6]) {
-                        TextView t = (TextView)findViewById(R.id.d3);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.d3);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                    if (arr[7]) {
-                        TextView t = (TextView)findViewById(R.id.d4);
-                        t.setBackgroundColor(Color.RED);
-                    } else {
-                        TextView t = (TextView)findViewById(R.id.d4);
-                        t.setBackgroundColor(Color.WHITE);
-                    }
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-
-
-            }
-        }, 0, 1000);
-
-        PillowSocket ps = PillowSocket.getInstance();
+        // Start the Runnable immediately
+        handler.post(runnable);
     }
 }
